2a7a861b6b400f0c04ae8e14c52b3837e2d8db49
Eng-Yasmina <aliyasoo70@gmail.com>
2025-05-09 13:14:56 +0300

    register widgets

--- lwn/footer.php
+++ lwn/footer.php
@@ -1,5 +1,24 @@
-<footer>
-        Copyright
+    <footer id="main-footer">
+        <div class="footer-container">
+            <div class="footer-column">
+                <?php if(is_active_sidebar('footer1')) : ?>
+                    <?php dynamic_sidebar('footer1'); ?>
+                <?php endif; ?> 
+            </div>
+            <div class="footer-column">
+                <?php if(is_active_sidebar('footer2')) : ?>
+                    <?php dynamic_sidebar('footer2'); ?>
+                <?php endif; ?> 
+            </div>
+            <div class="footer-column">
+                <?php if(is_active_sidebar('footer3')) : ?>
+                    <?php dynamic_sidebar('footer3'); ?>
+                <?php endif; ?> 
+            </div>
+            <div class="footer-column">
+                <p>Copyright</p>
+            </div>
+        </div>
     </footer>
     <?php wp_footer(); ?>
 </body>
--- lwn/functions.php
+++ lwn/functions.php
@@ -5,7 +5,7 @@ function enqueue_assets () {
     wp_style_add_data('lwn-style', 'rtl', 'replace');
     wp_enqueue_script('lwn-script', get_template_directory_uri() . '/assets/js/main.js', array(), '1.0.0', true);
 }
-//////////////////////////////
+//////////////MENUS////////////////
 add_action('init','lwn_register_menus');
 function lwn_register_menus() {
     register_nav_menus(
@@ -16,4 +16,35 @@ function lwn_register_menus() {
         )
     );
 }
-//////////////////////////////
+///////////////WIDGETS///////////////
+add_action( 'widgets_init', 'lwn__register_widgets' );
+function lwn__register_widgets() {
+    register_sidebar( array(
+        'name'          => __( 'Footer Column 1', 'textdomain' ),
+        'id'            => 'footer1',
+        'description'   => __( 'Add widgets here.', 'textdomain' ),
+        'before_widget' => '<div id="%1$s" class="widget lwn-widget %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4 class="widget-title">',
+        'after_title'   => '</h4>',
+    ) );
+    register_sidebar( array(
+        'name'          => __( 'Footer Column 2', 'textdomain' ),
+        'id'            => 'footer2',
+        'description'   => __( 'Add widgets here.', 'textdomain' ),
+        'before_widget' => '<div id="%1$s" class="widget lwn-widget %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4 class="widget-title">',
+        'after_title'   => '</h4>',
+    ) );
+    register_sidebar( array(
+        'name'          => __( 'Footer Column 3', 'textdomain' ),
+        'id'            => 'footer3',
+        'description'   => __( 'Add widgets here.', 'textdomain' ),
+        'before_widget' => '<div id="%1$s" class="widget lwn-widget %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4 class="widget-title">',
+        'after_title'   => '</h4>',
+    ) );
+}
+// ////////////////////////////
