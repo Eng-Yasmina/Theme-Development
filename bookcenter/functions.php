f3ea7fb9d16add838e3c5259b0b356c5b45396ab
Eng-Yasmina <aliyasoo70@gmail.com>
2025-05-11 21:24:03 +0300

    register taxonomy

--- bookcenter/functions.php
+++ bookcenter/functions.php
@@ -63,3 +63,71 @@ function bookcenter_register_book_custom_post_type() {
 }
 
 add_action( 'init', 'bookcenter_register_book_custom_post_type' );
+
+/**
+ * Create two taxonomies, genres and writers for the post type "book".
+ *
+ * @see register_post_type() for registering custom post types.
+ */
+function register_custom_taxonomies() {
+	// Add new taxonomy, make it hierarchical (like categories)
+	$booktype_labels = array(
+		'name'              => _x( 'Book Types', 'taxonomy general name', 'textdomain' ),
+		'singular_name'     => _x( 'Book Type', 'taxonomy singular name', 'textdomain' ),
+		'search_items'      => __( 'Search Book Types', 'textdomain' ),
+		'all_items'         => __( 'All Book Types', 'textdomain' ),
+		'parent_item'       => __( 'Parent Book Type', 'textdomain' ),
+		'parent_item_colon' => __( 'Parent Book Type:', 'textdomain' ),
+		'edit_item'         => __( 'Edit Book Type', 'textdomain' ),
+		'update_item'       => __( 'Update Book Type', 'textdomain' ),
+		'add_new_item'      => __( 'Add New Book Type', 'textdomain' ),
+		'new_item_name'     => __( 'New Book Type Name', 'textdomain' ),
+		'menu_name'         => __( 'Book Type', 'textdomain' ),
+	);
+
+	$booktype_args = array(
+		'hierarchical'      => true,
+		'labels'            => $booktype_labels,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'query_var'         => true,
+		'rewrite'           => array( 'slug' => 'book-type' ),
+	);
+
+	register_taxonomy( 'book_type', array( 'book' ), $booktype_args );
+
+
+	// Add new taxonomy, NOT hierarchical (like tags)
+	$writer_labels = array(
+		'name'                       => _x( 'Writers', 'taxonomy general name', 'textdomain' ),
+		'singular_name'              => _x( 'Writer', 'taxonomy singular name', 'textdomain' ),
+		'search_items'               => __( 'Search Writers', 'textdomain' ),
+		'popular_items'              => __( 'Popular Writers', 'textdomain' ),
+		'all_items'                  => __( 'All Writers', 'textdomain' ),
+		'parent_item'                => null,
+		'parent_item_colon'          => null,
+		'edit_item'                  => __( 'Edit Writer', 'textdomain' ),
+		'update_item'                => __( 'Update Writer', 'textdomain' ),
+		'add_new_item'               => __( 'Add New Writer', 'textdomain' ),
+		'new_item_name'              => __( 'New Writer Name', 'textdomain' ),
+		'separate_items_with_commas' => __( 'Separate writers with commas', 'textdomain' ),
+		'add_or_remove_items'        => __( 'Add or remove writers', 'textdomain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used writers', 'textdomain' ),
+		'not_found'                  => __( 'No writers found.', 'textdomain' ),
+		'menu_name'                  => __( 'Writers', 'textdomain' ),
+	);
+
+	$writer_args = array(
+		'hierarchical'          => false,
+		'labels'                => $writer_labels,
+		'show_ui'               => true,
+		'show_admin_column'     => true,
+		'update_count_callback' => '_update_post_term_count',
+		'query_var'             => true,
+		'rewrite'               => array( 'slug' => 'writer' ),
+	);
+
+	register_taxonomy( 'writer', 'book', $writer_args );
+}
+// hook into the init action and call create_book_taxonomies when it fires
+add_action( 'init', 'register_custom_taxonomies', 0 );
